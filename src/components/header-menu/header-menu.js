2441bf99a0c952e8de5d011927009c275b68888b
Woody Just <woody.just@gmail.com>
2017-05-13 19:14:26 +0400

    quotes, footer, adapt top menu

---
+++ src/components/header-menu/header-menu.js
@@ -0,0 +1,8 @@
+var headerMenu = document.querySelectorAll('.header-menu')[0];
+var burgerTumblr = document.querySelectorAll('.burger-tumblr')[0];
+var openMenu = () => {
+  headerMenu.classList.toggle('open');
+}
+
+burgerTumblr.addEventListener('click', openMenu);
+
